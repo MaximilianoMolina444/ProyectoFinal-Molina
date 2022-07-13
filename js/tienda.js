f29d9099461f59f8ecf72b1d45beab0d5c488464
Maximiliano Molina <maximilianomolina1998@gmail.com>
2022-07-13 18:43:43 -0300

    subiendo proyecto clase 15

--- js/tienda.js
+++ js/tienda.js
@@ -1,59 +1,3 @@
-let productos = [
-    {
-        id: "1",
-        nombre: "Remeras de bandas",
-        imagen: "https://i.ibb.co/sm0nB9s/Sin-t-tulo2.jpg",
-        precio: 2500
-    },
-    {
-        id: "2",
-        nombre: "Remeras de bandas",
-        imagen: "https://i.ibb.co/XJw53Sq/Sin-t-tulo3.jpg",
-        precio: 2000
-    },
-    {
-        id: "3",
-        nombre: "Barbijos",
-        imagen: "https://i.ibb.co/TqkK0wN/Sin-t-tulo4.jpg",
-        precio: 300
-    },
-    {
-        id: "4",
-        nombre: "Remeras personalizadas",
-        imagen: "https://i.ibb.co/4jJBvWc/sin-titulo5.jpg",
-        precio: 1500
-    },
-    {
-        id: "5",
-        nombre: "Remeras personalizadas",
-        imagen: "https://i.ibb.co/DQcjRvf/sin-titulo6.jpg",
-        precio: 1500
-    },
-    {
-        id: "6",
-        nombre: "Barbijos",
-        imagen: "https://i.ibb.co/p04Z84y/sin-titulo7.jpg",
-        precio: 200
-    },
-    {
-        id: "7",
-        nombre: "Pedidos especiales",
-        imagen: "https://i.ibb.co/8MDVDhb/musculosa.jpg",
-        precio: 1800
-    },
-    {
-        id: "8",
-        nombre: "Pedidos especiales",
-        imagen: "https://i.ibb.co/gWJzxsh/vampires.jpg",
-        precio: 1200
-    },
-    {
-        id: "9",
-        nombre: "Pedidos especiales",
-        imagen: "https://i.ibb.co/vQk7BKK/deftones.jpg",
-        precio: 3000
-    },
-];
 
 // setear carrito en localStorage------------------------------------------------------
 let carrito;
@@ -135,16 +79,22 @@ function alerta(){
 // añadir al carrito------------------------------------------------------
 function aniadirCarritoClick(e) {
     let iva = 1.21
-    const btn = e.target;
-    let idBoton = btn.getAttribute('id');
-    let prodEncontrado = productos.filter(prod => prod.id == idBoton);
-    let guardarCarrito = productos.find(prod => prod.id == idBoton)
-    const enCarrito = carrito.find(prod => prod.id == guardarCarrito.id)
-    let carritoFiltrado = carrito.filter(prod => prod.id != enCarrito.id)
-    !enCarrito? carrito.push({ ...prodEncontrado, cantidad: 1 }) : carrito = [...carritoFiltrado, { ...enCarrito, cantidad: enCarrito.cantidad + 1 }];
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-    aniadirProductos(prodEncontrado[0].precio*iva, prodEncontrado[0].imagen, prodEncontrado[0].nombre);
-    alerta()
+    fetch('../js/productos.json')
+    .then((res)=>res.json())
+    .then((data)=>{
+        
+        const btn = e.target;
+        let idBoton = btn.getAttribute('id');
+        let prodEncontrado = data.filter(prod => prod.id == idBoton);
+        let guardarCarrito = data.find(prod => prod.id == idBoton)
+        const enCarrito = carrito.find(prod => prod.id == guardarCarrito.id)
+        let carritoFiltrado = carrito.filter(prod => prod.id != enCarrito.id)
+        !enCarrito? carrito.push({ ...prodEncontrado, cantidad: 1 }) : carrito = [...carritoFiltrado, { ...enCarrito, cantidad: enCarrito.cantidad + 1 }];
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        aniadirProductos(prodEncontrado[0].precio*iva, prodEncontrado[0].imagen, prodEncontrado[0].nombre),
+        alerta()
+    })
+    
 }
 
 // Mostrar items en carrito------------------------------------------------------
